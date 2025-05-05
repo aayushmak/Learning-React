e6269ce9bc239faf5d9a381fd5da50c5c2c2f84b
aayushmak <aayushmakafle@gmail.com>
2025-05-05 21:32:47 +0545

    added dependencies

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,9 @@
+
+
+function App() {
+  return (
+    <h1>HIIIII | Aayushma Kafle</h1>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,9 @@
+
+function App() {
+
+  return (
+    <h1>HIIIIIII | AAyushma Kafle</h1>
+  )
+}
+
+export default App
