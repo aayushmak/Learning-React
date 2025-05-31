83b77c1d06a1bbae8702b20175c7c671319e3fa7
aayushmak <aayushmakafle@gmail.com>
2025-05-31 20:27:32 +0545

    started a project using router

---
+++ 07router/src/Layout.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Header from './components/header/header'
+import Footer from './components/footer/footer'
+import { Outlet } from 'react-router-dom'
+
+function Layout() {
+  return (
+    <div>
+      <Header/>
+      <Outlet/>
+      <Footer/>
+
+    </div>
+  )
+}
+
+export default Layout
