d3d95c6a74770ada09b414916a6f7df6924b4db1
aayushmak <aayushmakafle@gmail.com>
2025-06-04 14:48:15 +0545

    small changes

--- 09themeSwitcher/tailwind.config.js
+++ 09themeSwitcher/tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx,html}"],
-  darkMode: "class",
+  darkMode: "class",    //without this there is no change 
   theme: {
     extend: {},
   },
