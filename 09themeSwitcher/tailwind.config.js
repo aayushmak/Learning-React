36c4465daaf458447f69e8d515ab281d884e1fec
aayushmak <aayushmakafle@gmail.com>
2025-06-04 14:19:33 +0545

    made a theme witcher using context api

---
+++ 09themeSwitcher/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{js,ts,jsx,tsx,html}"],
+  darkMode: "class",
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
