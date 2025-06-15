f191b9f0f1b13d015f83ffa7e663d7819cbf3807
aayushmak <aayushmakafle@gmail.com>
2025-06-15 22:16:39 +0545

    added a new weather app

--- 06currconv/src/hooks/useCurrencyInfo.js
+++ 06currconv/src/hooks/useCurrencyInfo.js
@@ -1,16 +1,16 @@
-import {useEffect, useState} from "react"
+import { useEffect, useState } from "react";
 
+function useCurrencyInfo(currency) {
+    const [data, setData] = useState({});
 
-function useCurrencyInfo(currency){
-    const [data, setData] = useState({})
     useEffect(() => {
-        fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
-        .then((res) => res.json())
-        .then((res) => setData(res[currency]))
-        console.log(data);
-    }, [currency])
-    console.log(data);
-    return data
+        fetch(`https://api.exchangerate.host/latest?base=${currency}`)
+            .then((res) => res.json())
+            .then((res) => setData(res.rates))
+            .catch((error) => console.error("Error fetching exchange rates:", error));
+    }, [currency]);
+
+    return data;
 }
 
 export default useCurrencyInfo;
